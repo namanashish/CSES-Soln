f76a6a9cb4e65383d8e62e654b219adb1af04afb
namanashish <135628193+namanashish@users.noreply.github.com>
2025-07-25 02:13:19 +0530

    Apparently there is a underlying pattern

--- newspiral.cpp
+++ newspiral.cpp
@@ -1,17 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long find(long long x,long long y){
+/*long long find(long long x,long long y){
     long long rung=max(x,y);
     long long value=0;
     bool flag=(rung%2);
-
+    if(flag){
+        value=rung*rung;
+    }
+    else{
+        value=(rung-1)*(rung-1)+1;
+    }
     long long xs=1;
     long long ys=rung;
-    long long count=0;
 
     while(xs!=x || ys!=y){
-        
-        count++;
+        if(flag){
+            value--;
+        }
+        else{
+            value++;
+        }
         if(xs<rung){
             xs++;
         }
@@ -20,17 +28,12 @@ long long find(long long x,long long y){
         }
     }
     
-    if(flag){
-        value=rung*rung-count;
-    }
-    else{
-        value=(rung-1)*(rung-1)+count+1;
-    }
+    
 
     return value;
-}
+}*/
 int main(){
-    long long num;
+    /*long long num;
     cin>>num;
     vector<long long> ans;
     for(long long i=0;i<num;i++){
@@ -41,6 +44,35 @@ int main(){
     }
     for(long long i=0;i<num;i++){
         cout<<ans[i]<<endl;
+    }*/
+
+    int t=1;
+    cin>>t;
+    while(t--){
+        long long x,y;
+        cin>>x>>y;
+        if(x>y){
+            long long ans;
+            if(x%2==0){
+                ans=(x*x)-y+1;
+            }
+            else{
+                ans=((x-1)*(x-1))+y;
+                
+            }
+            cout<<ans<<endl;
+        }
+        else{
+            long long ans;
+            if(y%2==0){
+                ans=(y-1)*(y-1)+x;
+                
+            }
+            else{
+                ans=y*y - x+1;
+            }
+            cout<<ans<<endl;
+        }
     }
     return 0;
 }
